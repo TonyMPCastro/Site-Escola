543ff3f7b627f5d847d30e313b1c2336f587d717
TonyMPCastro <antonio.m.fg38@outlook.com>
2019-10-25 13:13:26 -0300

    atualização

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+			$url = (isset($_GET['url'])) ? $_GET['url']:'404.php';
+			$url = array_filter(explode('/',$url));
+			
+			$file = $url[0].'.php';
+			
+			if(is_file($file)){
+				include $file;
+			}else{
+				include '404.php';
+			}
+
+?>
